6da51f66fbf0359c9ac6caaa5c74839a58d7c42f
KANISHKA R <kanishka.r221@gmail.com>
2025-12-13 12:56:29 +0530

    Update 10. Testbench .sv

--- FULL ADDER VERIFICATION/10. Testbench .sv
+++ FULL ADDER VERIFICATION/10. Testbench .sv
@@ -1,1 +1,19 @@
+`include "interface.sv"
+`include "test.sv"
 
+module testbench;
+  inter interf();
+  test tst(interf);
+  
+  full_adder FA(
+    .a(interf.a),
+    .b(interf.b),
+    .c_in(interf.c_in),
+    .sum(interf.sum),
+    .c_out(interf.c_out)
+  );
+  initial begin 
+    $dumpfile("dump.vcd"); 
+    $dumpvars;
+  end
+endmodule
