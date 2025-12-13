a00acbc9c5cae209b3a0bf78515dab2d166fa649
KANISHKA R <kanishka.r221@gmail.com>
2025-12-13 12:50:57 +0530

    Add monitor class for transaction verification

--- FULL ADDER VERIFICATION/06. Monitor .sv
+++ FULL ADDER VERIFICATION/06. Monitor .sv
@@ -1,1 +1,26 @@
-
+class monitor;
+  transaction trans;
+  virtual inter interf;
+  mailbox monit_to_score;
+  
+  function new(virtual inter interf,mailbox monit_to_score);
+    this.interf = interf;
+    this.monit_to_score = monit_to_score;
+  endfunction
+  
+  task monitor();
+    trans = new();
+    repeat(5)begin
+      #2;
+      trans.a=interf.a;
+      trans.b=interf.b;
+      trans.c_in=interf.c_in;
+      trans.sum=interf.sum;
+      trans.c_out=interf.c_out;
+      monit_to_score.put(trans);
+      trans.display("MONITOR");
+      #1;
+    end
+  endtask
+endclass
+ 
