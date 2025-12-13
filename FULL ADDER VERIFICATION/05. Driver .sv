2cc45f25d98d4f2b68e6055eff6fd5f230bf5e76
KANISHKA R <kanishka.r221@gmail.com>
2025-12-13 12:50:27 +0530

    Update 05. Driver .sv

--- FULL ADDER VERIFICATION/05. Driver .sv
+++ FULL ADDER VERIFICATION/05. Driver .sv
@@ -1,1 +1,24 @@
-
+class driver;
+  virtual inter interf;
+  transaction trans;
+  mailbox gen_to_drive;
+  
+  function new (virtual inter interf,mailbox gen_to_drive);
+    this.interf = interf;
+    this.gen_to_drive = gen_to_drive;
+  endfunction
+  
+  task drive();
+    trans = new();
+    repeat(5)begin
+      #1;
+      gen_to_drive.get(trans);
+      interf.a = trans.a;
+      interf.b = trans.b;
+      interf.c_in = trans.c_in;
+      trans.display("DRIVER");
+      #2;
+    end
+  endtask
+endclass
+    
