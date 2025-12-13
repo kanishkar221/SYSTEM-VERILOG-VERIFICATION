b9936742c4328f601db2559f879b5e304411db02
KANISHKA R <kanishka.r221@gmail.com>
2025-12-13 12:49:32 +0530

    Add transaction class for full adder verification

--- FULL ADDER VERIFICATION/03. Transaction .sv
+++ FULL ADDER VERIFICATION/03. Transaction .sv
@@ -1,1 +1,14 @@
-
+class transaction;
+  rand bit a;
+  rand bit b;
+  rand bit c_in;
+       bit sum;
+       bit c_out;
+  
+  function void display (string name);
+    $display("%s",name);
+    $display("Time=%0t a=%b b=%b c_in=%b sum=%b c_out=%b",
+             $time,a,b,c_in,sum,c_out);
+  endfunction
+endclass
+  
