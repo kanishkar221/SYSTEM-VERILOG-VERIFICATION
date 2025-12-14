9f825c5d85c6849b10de89de4716a9b3d16a74c3
KANISHKA R <kanishka.r221@gmail.com>
2025-12-14 20:18:14 +0530

    Add scoreboard class for transaction verification

--- HALF ADDER VERIFICATION/07. Scoreboard .sv
+++ HALF ADDER VERIFICATION/07. Scoreboard .sv
@@ -1,1 +1,20 @@
-
+class scoreboard;
+  transaction trans;
+  mailbox monit_to_score;
+  
+  function new (mailbox monit_to_score);
+    this.monit_to_score = monit_to_score;
+  endfunction
+  
+  task score();
+    trans = new();
+    repeat(5)begin
+      monit_to_score.get(trans);
+      trans.display("SCOREBOARD");
+      if(((trans.a^trans.b)==trans.sum)&&((trans.a&trans.b)==trans.carry))
+        $display("PASS");
+      else
+        $display("FAIL");
+    end
+  endtask
+endclass
