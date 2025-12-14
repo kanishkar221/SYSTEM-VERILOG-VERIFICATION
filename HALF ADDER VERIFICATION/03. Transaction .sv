12de129e53b0b94588f0b5d49841d60141746d3c
KANISHKA R <kanishka.r221@gmail.com>
2025-12-14 20:16:31 +0530

    Add transaction class for half adder verification

--- HALF ADDER VERIFICATION/03. Transaction .sv
+++ HALF ADDER VERIFICATION/03. Transaction .sv
@@ -1,1 +1,12 @@
-
+class transaction;
+  rand bit a;
+  rand bit b;
+       bit sum;
+       bit carry;
+  
+  function void display(string name);
+    $display("%s",name);
+    $display("Time=%0t a=%b b=%b sum=%b carry=%b",$time,a,b,sum,carry);
+  endfunction
+endclass
+      
