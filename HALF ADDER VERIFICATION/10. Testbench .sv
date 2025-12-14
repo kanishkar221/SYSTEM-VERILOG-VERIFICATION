8f7a269264f27609f081a0ea0dc12273645dc312
KANISHKA R <kanishka.r221@gmail.com>
2025-12-14 20:19:56 +0530

    Update 10. Testbench .sv

--- HALF ADDER VERIFICATION/10. Testbench .sv
+++ HALF ADDER VERIFICATION/10. Testbench .sv
@@ -1,1 +1,18 @@
+`include "interface.sv"
+`include "test.sv"
 
+module testbench;
+  inter interf();
+  test tst(interf);
+  
+  half_adder HA(
+    .a(interf.a),
+    .b(interf.b),
+    .sum(interf.sum),
+    .carry(interf.carry)
+  );
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+endmodule
