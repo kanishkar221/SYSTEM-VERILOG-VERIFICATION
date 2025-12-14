bcb895257072c4315d4ff8cfd5a4e51a15fcd45a
KANISHKA R <kanishka.r221@gmail.com>
2025-12-14 20:16:53 +0530

    Update 04. Generator .sv

--- HALF ADDER VERIFICATION/04. Generator .sv
+++ HALF ADDER VERIFICATION/04. Generator .sv
@@ -1,1 +1,19 @@
-
+class generator;
+  transaction trans;
+  mailbox gen_to_drive;
+  
+  function new (mailbox gen_to_drive);
+    this.gen_to_drive = gen_to_drive;
+  endfunction
+  
+  task gen();
+    trans = new();
+    repeat(5)begin;
+      trans.randomize();
+      gen_to_drive.put(trans);
+      trans.display("GENERATOR");
+      #3;
+    end
+  endtask
+endclass
+  
