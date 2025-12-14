65d3c2edf3f08c1fef1bc0dfce2a1b909abe4d01
KANISHKA R <kanishka.r221@gmail.com>
2025-12-14 20:17:46 +0530

    Update 06. Monitor .sv

--- HALF ADDER VERIFICATION/06. Monitor .sv
+++ HALF ADDER VERIFICATION/06. Monitor .sv
@@ -1,1 +1,24 @@
-
+class monitor;
+  transaction trans;
+  virtual inter interf;
+  mailbox monit_to_score;
+  
+  function new (virtual inter interf, mailbox monit_to_score);
+    this.interf = interf;
+    this.monit_to_score = monit_to_score;
+  endfunction
+  
+  task monitor();
+    trans = new();
+    repeat(5)begin
+      #2;
+      trans.a = interf.a;
+      trans.b = interf.b;
+      trans.sum = interf.sum;
+      trans.carry = interf.carry;
+      monit_to_score.put(trans);
+      trans.display("MONITOR");
+      #1;
+    end
+  endtask
+endclass
