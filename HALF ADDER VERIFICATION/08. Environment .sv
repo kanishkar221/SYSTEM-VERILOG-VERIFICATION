49437c0c4b7e228f06ee3b1d4aa0900237ee5586
KANISHKA R <kanishka.r221@gmail.com>
2025-12-14 20:18:47 +0530

    Update 08. Environment .sv

--- HALF ADDER VERIFICATION/08. Environment .sv
+++ HALF ADDER VERIFICATION/08. Environment .sv
@@ -1,1 +1,38 @@
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
 
+class environment;
+  transaction trans;
+  generator gen;
+  driver drive;
+  monitor monit;
+  scoreboard score;
+  
+  mailbox gen_to_drive;
+  mailbox monit_to_score;
+  virtual inter interf;
+  
+  function new(virtual inter interf);
+    this.interf = interf;
+    gen_to_drive = new();
+    monit_to_score = new();
+    
+    gen = new(gen_to_drive);
+    drive = new(interf,gen_to_drive);
+    monit = new(interf,monit_to_score);
+    score = new(monit_to_score);
+    
+  endfunction
+  
+  task test();
+    fork
+      gen.gen();
+      drive.drive();
+      monit.monitor();
+      score.score();
+    join
+  endtask
+endclass
