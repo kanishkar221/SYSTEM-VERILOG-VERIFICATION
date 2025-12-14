587a097e03bb3addb0d6a35c83589da3f7e0f646
KANISHKA R <kanishka.r221@gmail.com>
2025-12-14 20:22:09 +0530

    Update 11. Output .sv

--- HALF ADDER VERIFICATION/11. Output .sv
+++ HALF ADDER VERIFICATION/11. Output .sv
@@ -1,1 +1,45 @@
-
+GENERATOR
+Time=0 a=1 b=1 sum=0 carry=0
+DRIVER
+Time=1 a=1 b=1 sum=0 carry=0
+MONITOR
+Time=2 a=1 b=1 sum=0 carry=1
+SCOREBOARD
+Time=2 a=1 b=1 sum=0 carry=1
+=============PASS=============
+GENERATOR
+Time=3 a=1 b=1 sum=0 carry=0
+DRIVER
+Time=4 a=1 b=1 sum=0 carry=0
+MONITOR
+Time=5 a=1 b=1 sum=0 carry=1
+SCOREBOARD
+Time=5 a=1 b=1 sum=0 carry=1
+=============PASS=============
+GENERATOR
+Time=6 a=1 b=1 sum=0 carry=0
+DRIVER
+Time=7 a=1 b=1 sum=0 carry=0
+MONITOR
+Time=8 a=1 b=1 sum=0 carry=1
+SCOREBOARD
+Time=8 a=1 b=1 sum=0 carry=1
+=============PASS=============
+GENERATOR
+Time=9 a=1 b=1 sum=0 carry=0
+DRIVER
+Time=10 a=1 b=1 sum=0 carry=0
+MONITOR
+Time=11 a=1 b=1 sum=0 carry=1
+SCOREBOARD
+Time=11 a=1 b=1 sum=0 carry=1
+=============PASS=============
+GENERATOR
+Time=12 a=0 b=0 sum=0 carry=0
+DRIVER
+Time=13 a=0 b=0 sum=0 carry=0
+MONITOR
+Time=14 a=0 b=0 sum=0 carry=0
+SCOREBOARD
+Time=14 a=0 b=0 sum=0 carry=0
+=============PASS=============
