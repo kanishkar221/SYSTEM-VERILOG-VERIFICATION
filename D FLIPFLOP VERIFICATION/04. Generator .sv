be5cab6087436dd04cbc3f2b799938e0e0d4b8b7
KANISHKA R <kanishka.r221@gmail.com>
2026-01-19 22:20:39 +0530

    Add generator class for transaction generation

--- D FLIPFLOP VERIFICATION/04. Generator .sv
+++ D FLIPFLOP VERIFICATION/04. Generator .sv
@@ -1,1 +1,20 @@
-
+class generator;
+  transaction trans;
+  mailbox gen_to_driv;
+  event trigger;
+  
+  function new(mailbox gen_to_driv);
+    this.trans = trans;
+    this.gen_to_driv = gen_to_driv;
+  endfunction
+  
+  task gen();
+    trans = new();
+    repeat(5) begin
+      void'(trans.randomize());
+      gen_to_driv.put(trans);
+      trans.disp("GENERATOR");
+      @trigger;
+    end
+  endtask
+endclass    
