c9a0db70d7c8ee3581dfcfb187fe1e6fdf9c91a7
KANISHKA R <kanishka.r221@gmail.com>
2026-01-19 22:20:11 +0530

    Update 03. Transaction .sv

--- D FLIPFLOP VERIFICATION/03. Transaction .sv
+++ D FLIPFLOP VERIFICATION/03. Transaction .sv
@@ -1,1 +1,10 @@
-
+class transaction;
+  bit rst;
+  rand bit d;
+  bit q;
+  
+  function void disp(string message);
+    $display("%s", message);
+    $display("Time = %0t | rst=%b | d=%b | q=%b",$time,rst,d,q);
+  endfunction
+endclass
