58b9f41ffed24f0d68c60fdb28702371039a191a
KANISHKA R <kanishka.r221@gmail.com>
2026-01-19 22:21:24 +0530

    Update 06. Monitor .sv

--- D FLIPFLOP VERIFICATION/06. Monitor .sv
+++ D FLIPFLOP VERIFICATION/06. Monitor .sv
@@ -1,1 +1,22 @@
-
+class monitor;
+  transaction trans;
+  mailbox moni_to_scor;
+  virtual inter inf;
+  
+  function new(virtual inter inf, mailbox moni_to_scor);
+    this.inf = inf;
+    this.moni_to_scor = moni_to_scor;
+  endfunction
+  
+  task moni();
+    trans = new();
+    repeat(5) begin
+      @(posedge inf.clk);
+      trans.rst = inf.rst;
+      trans.d = inf.d;
+      trans.q = inf.q;
+      moni_to_scor.put(trans);
+      trans.disp("MONITOR");
+    end
+  endtask
+endclass
