095e280f9883b8c5ee6ea7d184f09399cc176e5f
KANISHKA R <kanishka.r221@gmail.com>
2026-01-19 22:21:50 +0530

    Update 07. Scoreboard .sv

--- D FLIPFLOP VERIFICATION/07. Scoreboard .sv
+++ D FLIPFLOP VERIFICATION/07. Scoreboard .sv
@@ -1,1 +1,23 @@
-
+class scoreboard;
+  transaction trans;
+  mailbox moni_to_scor;
+  event trigger;
+  
+  function new(mailbox moni_to_scor);
+    this.moni_to_scor = moni_to_scor;
+  endfunction
+  
+  task scor();
+    trans = new();
+    repeat(5) begin
+      moni_to_scor.get(trans);
+      trans.disp("SCOREBOARD");
+      
+      if((trans.rst == 1) && (trans.q == 0) || (trans.rst == 0) && (trans.q == trans.d))
+        $display("-------PASS------");
+      else
+        $display("-------FAIL------");
+      -> trigger;
+    end
+  endtask
+endclass
