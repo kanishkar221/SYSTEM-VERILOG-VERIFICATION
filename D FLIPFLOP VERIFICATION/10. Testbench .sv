aaedc9139ea620dbab71a6e8e93c587f50c3d235
KANISHKA R <kanishka.r221@gmail.com>
2026-01-19 22:23:09 +0530

    Update 10. Testbench .sv

--- D FLIPFLOP VERIFICATION/10. Testbench .sv
+++ D FLIPFLOP VERIFICATION/10. Testbench .sv
@@ -1,1 +1,27 @@
+`include "interface.sv"
+`include "test.sv"
 
+module d_ff_tb;
+  inter inf();
+  test tst(inf);
+  
+   d_flipflop dff(.clk(inf.clk), .rst(inf.rst), .d(inf.d), .q(inf.q));
+  
+  initial begin
+    inf.clk = 0;
+  end
+  
+  always #5 inf.clk = ~ inf.clk;
+  
+  initial begin
+    inf.rst = 1;
+    #30; inf.rst = 0;
+  end
+  
+  initial begin
+    $dumpfile("dumpfile.vcd");
+    $dumpvars;
+    #200;
+    $finish;
+  end
+endmodule
