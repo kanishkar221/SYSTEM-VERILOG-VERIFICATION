9a6e5b22e1b835459fe494f94fdfb2393d0ac1f0
KANISHKA R <kanishka.r221@gmail.com>
2026-02-03 18:50:50 +0530

    Update 06. Monitor .sv

--- MEMORY/06. Monitor .sv
+++ MEMORY/06. Monitor .sv
@@ -1,1 +1,24 @@
-
+class monitor;
+  transaction trans;
+  virtual inter interf;
+  mailbox monit_to_score;
+  
+  function new(virtual inter interf, mailbox monit_to_score);
+    this.interf = interf;
+    this.monit_to_score = monit_to_score;
+  endfunction
+  
+  task monitor();
+    trans = new();
+    repeat(20)begin
+      @(posedge interf.clk);
+      trans.wr = interf.wr;
+      trans.d_in = interf.d_in;
+      trans.addr = interf.addr;
+      trans.d_out = interf.d_out;
+      monit_to_score.put(trans);
+      trans.display("MONITOR");
+    end
+  endtask
+  
+endclass
