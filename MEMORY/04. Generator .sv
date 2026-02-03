8554fdf55b52336e595285b202ea234fcdc26f22
KANISHKA R <kanishka.r221@gmail.com>
2026-02-03 18:49:37 +0530

    Update 04. Generator .sv

--- MEMORY/04. Generator .sv
+++ MEMORY/04. Generator .sv
@@ -1,1 +1,22 @@
-
+class generator;
+  transaction trans;
+  mailbox gen_to_drive;
+  event trigger;
+  
+  function new(mailbox gen_to_drive);
+    this.trans = trans;
+    this.gen_to_drive = gen_to_drive;
+  endfunction
+  
+  task gen();
+    trans = new();
+    repeat(20) begin
+      trans.randomize();
+      //void'(trans.randomize());
+      gen_to_drive.put(trans);
+      trans.display("GENERATOR");
+      @ trigger;
+    end
+  endtask
+  
+endclass
