c2fdf6fc963bf0ed052f631e77a19618c8c1a27c
KANISHKA R <kanishka.r221@gmail.com>
2026-02-03 18:54:06 +0530

    Update 10. Testbench .sv

--- MEMORY/10. Testbench .sv
+++ MEMORY/10. Testbench .sv
@@ -1,1 +1,24 @@
+`include "interface.sv"
+`include "test.sv"
 
+module tb;
+  inter interf();
+  test tst(interf);
+  
+  ram uut(.clk(interf.clk),
+          .wr(interf.wr),
+          .d_in(interf.d_in),
+          .addr(interf.addr),
+          .d_out(interf.d_out)
+         );
+  
+  always #5 interf.clk = ~interf.clk;
+  
+  initial begin
+    interf.clk = 0;
+    $dumpfile("dumpfile.vcd");
+    $dumpvars;
+    #500;
+    $finish;
+  end
+endmodule
