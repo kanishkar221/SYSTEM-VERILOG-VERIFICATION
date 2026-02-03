b294595eb9773d168b69523ca73de176e7b3a0eb
KANISHKA R <kanishka.r221@gmail.com>
2026-02-03 18:47:51 +0530

    Update 01. Design .sv

--- MEMORY/01. Design .sv
+++ MEMORY/01. Design .sv
@@ -1,1 +1,17 @@
-
+module ram (
+  input logic clk,
+  input logic wr,
+  input logic [7:0] d_in,
+  input logic [3:0] addr,
+  output logic [7:0] d_out
+);
+  
+  logic [7:0] mem [15:0];
+  
+  always @ (posedge clk) begin
+    if(wr)
+      mem[addr] <= d_in;
+    else
+      d_out <= mem[addr];
+  end
+endmodule
