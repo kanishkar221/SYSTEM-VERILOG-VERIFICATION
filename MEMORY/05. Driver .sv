3f5812e6a961510d41d8bf6580faab944a107023
KANISHKA R <kanishka.r221@gmail.com>
2026-02-03 18:50:19 +0530

    Update 05. Driver .sv

--- MEMORY/05. Driver .sv
+++ MEMORY/05. Driver .sv
@@ -1,1 +1,23 @@
-
+class driver;
+  virtual inter interf;
+  transaction trans;
+  mailbox gen_to_drive;
+  
+  function new(virtual inter interf, mailbox gen_to_drive);
+    this.interf = interf;
+    this.gen_to_drive = gen_to_drive;
+  endfunction
+  
+  task drive();
+    trans = new();
+    repeat(20)begin
+      gen_to_drive.get(trans);
+      interf.wr <= trans.wr;
+      interf.d_in <= trans.d_in;
+      interf.addr <= trans.addr;
+      trans.display("DRIVER");
+      
+    end
+  endtask
+  
+endclass
