5eda2d840eda2d19cc53508141a425b7f879c0bf
KANISHKA R <kanishka.r221@gmail.com>
2026-02-03 18:49:11 +0530

    Update 03. Transaction .sv

--- MEMORY/03. Transaction .sv
+++ MEMORY/03. Transaction .sv
@@ -1,1 +1,16 @@
-
+class transaction;
+  
+  rand bit wr;
+  rand bit [7:0] d_in;
+  rand bit [3:0] addr;
+  bit [7:0] d_out;
+  
+  constraint wr_equal {
+    wr dist { 1:=10, 0:=10};}
+  
+  function void display(string name);
+    $display("%s", name);
+    $display("Time=%0t  wr=%0d  d_in=%0d  addr=%0d  d_out=%0b",$time, wr, d_in, addr, d_out);
+  endfunction
+  
+endclass
