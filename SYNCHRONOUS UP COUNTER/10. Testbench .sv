d5460b91e7a5f4d70a7b9e46c2c5330daf699866
KANISHKA R <kanishka.r221@gmail.com>
2026-02-04 22:13:03 +0530

    Update 10. Testbench .sv

--- SYNCHRONOUS UP COUNTER/10. Testbench .sv
+++ SYNCHRONOUS UP COUNTER/10. Testbench .sv
@@ -1,1 +1,32 @@
+`include "interface.sv"
+`include "test.sv"
 
+module testbench;
+  inter interf();
+  test tst(interf);
+  
+  sync_up_counter dut(
+    .clk(interf.clk),
+    .reset(interf.reset),
+    .enable(interf.enable),
+    .count(interf.count)
+  );
+  
+  initial begin
+    interf.clk = 0;
+  end
+  
+  always #5 interf.clk = ~interf.clk;
+  
+  initial begin
+    interf.reset = 1;
+    #5; interf.reset = 0;
+  end
+  
+  initial begin
+    $dumpfile("dumpfile.vcd");
+    $dumpvars;
+    #100;
+    $finish;
+  end
+endmodule
