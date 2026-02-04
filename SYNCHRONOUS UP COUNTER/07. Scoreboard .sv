5dc55fcc27499c23b162c1ae2dc04205a6998195
KANISHKA R <kanishka.r221@gmail.com>
2026-02-04 22:11:37 +0530

    Update 07. Scoreboard .sv

--- SYNCHRONOUS UP COUNTER/07. Scoreboard .sv
+++ SYNCHRONOUS UP COUNTER/07. Scoreboard .sv
@@ -1,1 +1,33 @@
-
+class scoreboard;
+  transaction trans;
+  mailbox monit_to_score;
+  //event trigger;
+    
+  bit [3:0] exp_count;
+  
+  function new(mailbox monit_to_score);
+    this.monit_to_score = monit_to_score;
+    exp_count = 0;
+  endfunction
+  
+  task score();
+    trans = new();
+    repeat(10)begin
+      monit_to_score.get(trans);
+      trans.display("SOREBOARD");
+      
+      if(trans.reset)
+        exp_count = 0;
+      else if (trans.enable)
+        exp_count = exp_count + 1'b1;    
+      
+      if(trans.count == exp_count)
+        $display("PASS ----> reset=%0b  enable=%0b  Actual count=%0d  Expected count=%0d", trans.reset, trans.enable,  trans.count, exp_count);
+      else
+        $display("FAIL ----> reset=%0b  enable=%0b  Actual count=%0d Expected count=%0d", trans.reset, trans.enable, trans.count, exp_count);
+      $display("----------------------------------------------------------------");                                
+      //-> trigger;
+    end
+  endtask
+  
+endclass
