c9cccb58b87880b9de276b43c762cdc9bf4e90ce
KANISHKA R <kanishka.r221@gmail.com>
2026-02-04 22:09:54 +0530

    Update 03. Transaction .sv

--- SYNCHRONOUS UP COUNTER/03. Transaction .sv
+++ SYNCHRONOUS UP COUNTER/03. Transaction .sv
@@ -1,1 +1,12 @@
-
+class transaction;
+  bit clk;
+  bit reset;
+  rand bit enable;
+  bit [3:0] count;
+  
+  function void display(string name);
+    $display("%s", name);
+    $display("Time=%0t  reset=%0b  enable=%0b  count=%0d",$time, reset, enable, count);
+  endfunction
+  
+endclass
