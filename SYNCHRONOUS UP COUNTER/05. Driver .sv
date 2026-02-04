11032da1d0c988c7131b0ac1827e74ade6750977
KANISHKA R <kanishka.r221@gmail.com>
2026-02-04 22:10:47 +0530

    Update 05. Driver .sv

--- SYNCHRONOUS UP COUNTER/05. Driver .sv
+++ SYNCHRONOUS UP COUNTER/05. Driver .sv
@@ -1,1 +1,22 @@
-
+class driver;
+  virtual inter interf;
+  transaction trans;
+  mailbox gen_to_drive;
+  
+  function new(virtual inter interf, mailbox gen_to_drive);
+    this.interf = interf;
+    this.gen_to_drive = gen_to_drive;
+  endfunction
+  
+  task drive();
+    trans = new();
+    repeat(10)begin
+      gen_to_drive.get(trans);
+      interf.enable = trans.enable;
+      @(negedge interf.clk);
+      //@(posedge interf.clk);
+      trans.display("DRIVER");
+    end
+  endtask
+  
+endclass
