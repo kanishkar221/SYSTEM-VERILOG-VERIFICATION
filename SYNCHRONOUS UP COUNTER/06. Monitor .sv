bdc86aff8f6d6ff415dc73191b237f2cb364d39d
KANISHKA R <kanishka.r221@gmail.com>
2026-02-04 22:11:10 +0530

    Update 06. Monitor .sv

--- SYNCHRONOUS UP COUNTER/06. Monitor .sv
+++ SYNCHRONOUS UP COUNTER/06. Monitor .sv
@@ -1,1 +1,23 @@
-
+class monitor;
+  transaction trans;
+  virtual inter interf;
+  mailbox monit_to_score;
+  
+  function new(virtual inter interf, mailbox monit_to_score);
+    this.interf = interf;
+    this.monit_to_score = monit_to_score;
+  endfunction
+  
+  task monitor();
+    trans = new();
+    repeat(10)begin
+      @(posedge interf.clk);
+      trans.reset = interf.reset;
+      trans.enable = interf.enable;
+      trans.count = interf.count;
+      monit_to_score.put(trans);
+      trans.display("MONITOR");
+    end
+  endtask
+  
+endclass
