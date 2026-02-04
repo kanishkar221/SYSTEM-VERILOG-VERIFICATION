1f40b26a8b7975bc3cebe9510c920f75ba84d31e
KANISHKA R <kanishka.r221@gmail.com>
2026-02-04 22:10:15 +0530

    Update 04. Generator .sv

--- SYNCHRONOUS UP COUNTER/04. Generator .sv
+++ SYNCHRONOUS UP COUNTER/04. Generator .sv
@@ -1,1 +1,21 @@
-
+class generator;
+  transaction trans;
+  mailbox gen_to_drive;
+  //event trigger;
+    
+  function new(mailbox gen_to_drive);
+    this.gen_to_drive = gen_to_drive;
+  endfunction
+  
+  task gen();
+    trans = new();
+    repeat(10)begin
+      trans.randomize();
+      gen_to_drive.put(trans);
+      trans.display("GENERATOR");
+      #10;
+    end
+  endtask
+  
+endclass
+  
