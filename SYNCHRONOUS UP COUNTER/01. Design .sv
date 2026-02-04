f58d0b099cdd53486bfc5ba698959908fc6ea4c5
KANISHKA R <kanishka.r221@gmail.com>
2026-02-04 22:08:49 +0530

    Update 01. Design .sv

--- SYNCHRONOUS UP COUNTER/01. Design .sv
+++ SYNCHRONOUS UP COUNTER/01. Design .sv
@@ -1,1 +1,15 @@
+module sync_up_counter (
+  input  logic clk,
+  input  logic reset,
+  input  logic enable,
+  output logic [3:0] count
+);
 
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset)
+      count <= 4'b0000;
+    else if (enable)
+      count <= count + 1'b1;
+  end
+
+endmodule
