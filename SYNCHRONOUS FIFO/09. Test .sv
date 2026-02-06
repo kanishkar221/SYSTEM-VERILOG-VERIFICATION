4a02711a03512a79caeb3275a5e853dccbe33241
KANISHKA R <kanishka.r221@gmail.com>
2026-02-06 21:49:32 +0530

    Update 09. Test .sv

--- SYNCHRONOUS FIFO/09. Test .sv
+++ SYNCHRONOUS FIFO/09. Test .sv
@@ -1,1 +1,9 @@
+`include "environment.sv"
 
+program test #(parameter DATA_WIDTH=8,DEPTH=8)(inter interf);
+  environment #(DATA_WIDTH, DEPTH) env;
+  initial begin
+    env = new(interf);
+    env.test();
+  end
+endprogram
