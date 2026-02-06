1af37f36a62e8a445ac455900310d4ffef22d038
KANISHKA R <kanishka.r221@gmail.com>
2026-02-06 21:45:46 +0530

    Update 04. Generator .sv

--- SYNCHRONOUS FIFO/04. Generator .sv
+++ SYNCHRONOUS FIFO/04. Generator .sv
@@ -1,1 +1,21 @@
 
+class generator #(parameter DATA_WIDTH = 8);
+  transaction #(DATA_WIDTH) trans;
+  mailbox gen_to_drive;
+  
+  function new(mailbox gen_to_drive);
+    this.gen_to_drive = gen_to_drive;
+  endfunction
+  
+  task gen();
+    trans = new();
+    repeat(10)begin
+      void'(trans.randomize());    
+      //trans.randomize();
+      gen_to_drive.put(trans);
+      trans.display("GENERATOR");
+      #10;
+    end
+  endtask
+  
+endclass
