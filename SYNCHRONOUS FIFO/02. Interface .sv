382efd539aa4c7df1ade33139a3e0758038291fa
KANISHKA R <kanishka.r221@gmail.com>
2026-02-06 21:44:16 +0530

    Update 02. Interface .sv

--- SYNCHRONOUS FIFO/02. Interface .sv
+++ SYNCHRONOUS FIFO/02. Interface .sv
@@ -1,1 +1,10 @@
-
+interface inter #(parameter DATA_WIDTH = 8, DEPTH = 8);
+  logic clk;
+  logic rst_n;
+  logic w_en;
+  logic r_en;
+  logic [DATA_WIDTH-1:0] data_in;
+  logic [DATA_WIDTH-1:0] data_out;
+  logic full;
+  logic empty;
+endinterface
