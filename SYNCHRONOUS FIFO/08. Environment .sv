77e2fe2989651036a68f1e09d6134c7bb4c5c0e2
KANISHKA R <kanishka.r221@gmail.com>
2026-02-06 21:48:26 +0530

    Update 08. Environment .sv

--- SYNCHRONOUS FIFO/08. Environment .sv
+++ SYNCHRONOUS FIFO/08. Environment .sv
@@ -1,1 +1,40 @@
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
 
+class environment #(parameter DATA_WIDTH = 8, DEPTH = 8);
+  transaction #(DATA_WIDTH) trans;
+  generator   #(DATA_WIDTH) gen;
+  driver      #(DATA_WIDTH, DEPTH) drive;
+  monitor     #(DATA_WIDTH, DEPTH) monit;
+  scoreboard  #(DATA_WIDTH) score;
+  
+  mailbox gen_to_drive;
+  mailbox monit_to_score;
+  
+  virtual inter #(DATA_WIDTH, DEPTH) interf;
+  
+  function new(virtual inter #(DATA_WIDTH, DEPTH) interf);
+    this.interf = interf;
+    gen_to_drive = new();
+    monit_to_score = new();
+    
+    gen = new(gen_to_drive);
+    drive = new(interf, gen_to_drive);
+    monit = new(interf, monit_to_score);
+    score = new(monit_to_score);
+    
+  endfunction
+  
+  task test();
+    fork
+      gen.gen();
+      drive.drive();
+      monit.monitor();
+      score.score();
+    join
+  endtask
+  
+endclass
